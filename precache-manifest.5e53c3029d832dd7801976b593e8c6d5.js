2c43f4b437745cd43390ab6b6c68db60be668434
Michael Grandison <african.genetic.survival@gmail.com>
2020-08-15 23:41:20 -0500

    build for client demo

---
+++ precache-manifest.5e53c3029d832dd7801976b593e8c6d5.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df5c7049507754dfa33ca0893f19595",
+    "url": "/soulfuljoy_site_v2/index.html"
+  },
+  {
+    "revision": "cb2b749cb07710600257",
+    "url": "/soulfuljoy_site_v2/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "96eedb649936ca375c4c",
+    "url": "/soulfuljoy_site_v2/static/css/main.725560f5.chunk.css"
+  },
+  {
+    "revision": "cb2b749cb07710600257",
+    "url": "/soulfuljoy_site_v2/static/js/2.194d707f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/soulfuljoy_site_v2/static/js/2.194d707f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96eedb649936ca375c4c",
+    "url": "/soulfuljoy_site_v2/static/js/main.640f5062.chunk.js"
+  },
+  {
+    "revision": "7a93d4bde59915966a41",
+    "url": "/soulfuljoy_site_v2/static/js/runtime-main.eb1c5830.js"
+  },
+  {
+    "revision": "78b681ccd1cb098d42f28b2b71c469cf",
+    "url": "/soulfuljoy_site_v2/static/media/Splash1.78b681cc.jpg"
+  },
+  {
+    "revision": "db5e4bd8844897ac5d8fcb01584ec52e",
+    "url": "/soulfuljoy_site_v2/static/media/carousel_1.db5e4bd8.png"
+  },
+  {
+    "revision": "7e243159f72b3b7568b57f0b58a51936",
+    "url": "/soulfuljoy_site_v2/static/media/carousel_2.7e243159.jpg"
+  },
+  {
+    "revision": "e59d83817f32d599e1b5c8b748c93e29",
+    "url": "/soulfuljoy_site_v2/static/media/carousel_3.e59d8381.jpg"
+  },
+  {
+    "revision": "b705822e495ce3ba9063e32222b543fe",
+    "url": "/soulfuljoy_site_v2/static/media/carousel_4.b705822e.jpg"
+  },
+  {
+    "revision": "69222841cd050ea717f36be5b405fcab",
+    "url": "/soulfuljoy_site_v2/static/media/experienceHeader.69222841.jpg"
+  },
+  {
+    "revision": "8a5e666d00090f52895a512aca601ce2",
+    "url": "/soulfuljoy_site_v2/static/media/immerse.8a5e666d.jpg"
+  },
+  {
+    "revision": "99eaad976353e8320e883f22619ea1b5",
+    "url": "/soulfuljoy_site_v2/static/media/improve.99eaad97.jpeg"
+  },
+  {
+    "revision": "e8fcf325310fa40ba8bde42260a9bf0b",
+    "url": "/soulfuljoy_site_v2/static/media/incorporate.e8fcf325.jpg"
+  },
+  {
+    "revision": "c155a4e06fa2cc715e385737f66f3239",
+    "url": "/soulfuljoy_site_v2/static/media/inspire.c155a4e0.jpg"
+  },
+  {
+    "revision": "d554352750382d9db0b514c4daed19a9",
+    "url": "/soulfuljoy_site_v2/static/media/invigorate.d5543527.jpg"
+  },
+  {
+    "revision": "0f63f80fd74ecc2035e6ada6b580ddbd",
+    "url": "/soulfuljoy_site_v2/static/media/joyBackground.0f63f80f.svg"
+  },
+  {
+    "revision": "dd333a9e9d68db4410736d5e0ec381e6",
+    "url": "/soulfuljoy_site_v2/static/media/joyHeader.dd333a9e.jpg"
+  },
+  {
+    "revision": "0d3437275dad65b6974b0f3cb6b18f4d",
+    "url": "/soulfuljoy_site_v2/static/media/kwayera.0d343727.jpeg"
+  },
+  {
+    "revision": "e6c88f0edf5c24fb0c21902986148a69",
+    "url": "/soulfuljoy_site_v2/static/media/location_1.e6c88f0e.jpg"
+  },
+  {
+    "revision": "570466a1f5bd5fe5f88e7d7cbe4c805c",
+    "url": "/soulfuljoy_site_v2/static/media/location_2.570466a1.jpg"
+  },
+  {
+    "revision": "9d7bcf3671310a3c50943c88073751ba",
+    "url": "/soulfuljoy_site_v2/static/media/location_3.9d7bcf36.jpg"
+  },
+  {
+    "revision": "1693b41645482b9d2be276b333d111ce",
+    "url": "/soulfuljoy_site_v2/static/media/location_4.1693b416.jpg"
+  },
+  {
+    "revision": "ffeb3f9254b082cf0b08ea6a27d7502f",
+    "url": "/soulfuljoy_site_v2/static/media/location_5.ffeb3f92.jpg"
+  },
+  {
+    "revision": "84ad2044025d4166b4c6eab31aee8dd9",
+    "url": "/soulfuljoy_site_v2/static/media/location_6.84ad2044.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/soulfuljoy_site_v2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2b9264b83ef602a2163687b57605504c",
+    "url": "/soulfuljoy_site_v2/static/media/mobile_carousel_1.2b9264b8.jpg"
+  },
+  {
+    "revision": "2f1f02e7106ac964f770a7b6c6a9e0d6",
+    "url": "/soulfuljoy_site_v2/static/media/mobile_carousel_2.2f1f02e7.jpg"
+  },
+  {
+    "revision": "bd5b71e8158adea76bbc50be50c3665f",
+    "url": "/soulfuljoy_site_v2/static/media/mobile_carousel_3.bd5b71e8.jpg"
+  },
+  {
+    "revision": "036499b4a1ccf7c7f8a28623880a616f",
+    "url": "/soulfuljoy_site_v2/static/media/mobile_carousel_4.036499b4.jpg"
+  },
+  {
+    "revision": "225468713d13cb02ba6a0f359add6751",
+    "url": "/soulfuljoy_site_v2/static/media/mobile_splash_bg.22546871.jpg"
+  },
+  {
+    "revision": "2001a500b747ee560142972df54a561f",
+    "url": "/soulfuljoy_site_v2/static/media/pathBackground.2001a500.svg"
+  },
+  {
+    "revision": "a42ed6089dcc73f54a142a7a6e085169",
+    "url": "/soulfuljoy_site_v2/static/media/pathHeader.a42ed608.jpg"
+  },
+  {
+    "revision": "581b97df6a5dbeeef46cb31fa2dbe08d",
+    "url": "/soulfuljoy_site_v2/static/media/teamBackground.581b97df.svg"
+  },
+  {
+    "revision": "ec34d8158ed12f2b1507848cce26a766",
+    "url": "/soulfuljoy_site_v2/static/media/teamHeader.ec34d815.png"
+  },
+  {
+    "revision": "c40bb1945c475d0f5ec9b6e0d4e6b874",
+    "url": "/soulfuljoy_site_v2/static/media/team_1_kwayera.c40bb194.jpeg"
+  },
+  {
+    "revision": "e1a82cdbcaae20ffb534883371c4173a",
+    "url": "/soulfuljoy_site_v2/static/media/team_2_ade.e1a82cdb.jpg"
+  },
+  {
+    "revision": "5e193dc39ea66d5da4b9a7c5407d20b5",
+    "url": "/soulfuljoy_site_v2/static/media/team_3_khadijah.5e193dc3.jpg"
+  },
+  {
+    "revision": "7922d03a93b429c3b75ca5d849a1347e",
+    "url": "/soulfuljoy_site_v2/static/media/team_4_kwasausya.7922d03a.jpg"
+  },
+  {
+    "revision": "6eb44ed322b23c59f452ec72de92c025",
+    "url": "/soulfuljoy_site_v2/static/media/team_5_perdella.6eb44ed3.jpg"
+  },
+  {
+    "revision": "7c0a1fa7c676ce2557d0b590868efdb9",
+    "url": "/soulfuljoy_site_v2/static/media/wht_ico.7c0a1fa7.png"
+  }
+]);
